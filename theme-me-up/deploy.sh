4aa4f659f700c74ec63d7c702d8412631f65e953
petrspelos <petrspelos@users.noreply.github.com>
2020-02-15 19:20:58 +0100

    Remove unused deploy script

--- theme-me-up/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-dotnet publish -c Release -r linux-x64 --self-contained false
-cp /home/user/Repos/petrspelos/theme-me-up/theme-me-up/bin/Release/netcoreapp3.0/linux-x64/publish/* /home/user/tools
