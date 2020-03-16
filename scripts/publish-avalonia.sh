7b1f9dccaef7390d1225da52ef6f173dbfc297d1
petrspelos <petrspelos@users.noreply.github.com>
2020-03-16 17:14:30 +0100

    Add deployment for Avalonia

---
+++ scripts/publish-avalonia.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+dotnet publish ./src/ThemeMeUp.Avalonia -c Release -r linux-x64 /p:PublishSingleFile=true
+cd src/ThemeMeUp.Avalonia/bin/Release/netcoreapp3.0/linux-x64/publish
+mv ThemeMeUp.Avalonia theme-me-up
+
