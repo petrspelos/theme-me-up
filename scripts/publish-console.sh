0d33dbedff5bc18a5d65e015e31704154951f853
petrspelos <petrspelos@users.noreply.github.com>
2020-03-27 14:40:44 +0100

    Update publish script to copy into bin

--- scripts/publish-console.sh
+++ scripts/publish-console.sh
@@ -3,4 +3,5 @@ dotnet publish ./src/ThemeMeUp.ConsoleApp -c Release -r linux-x64 /p:PublishSing
 cd src/ThemeMeUp.ConsoleApp/bin/Release/netcoreapp3.1/linux-x64/publish
 mv ThemeMeUp.ConsoleApp theme-me-up
 rm ThemeMeUp.ConsoleApp.pdb
+sudo cp theme-me-up /bin/theme-me-up
 
